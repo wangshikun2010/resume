ed5ea7c10f9761c809579a9c394f2e71a158e78f
wangshikun2010 <wangshikun2010@gmail.com>
2013-07-17 10:29:48 +0800

    update resume

--- mobile.php
+++ mobile.php
@@ -15,30 +15,25 @@
 
     <div data-role="content">
         <p class="version">前端工程师</p>
-        <p id="contactme">
-            <a href="mailto:wangshikun2010@gmail.com" title="wangshikun2010@gmail.com"><img src="img/email.png" alt="给我发邮件"></a>
-            <a href="tel:18810331153"><img src="img/phone.png" alt="给我打电话"></a>
-        </p>
 
         <div id="homeheader">
             <p>王仕昆<small>（男·20）</small></p>
         </div>
 
-        <p class="intro">自学前端技术，制作多个前端应用技术小项目；具有良好的编码和重构习惯，熟悉面向对象和设计模式。<span class="label label-info">求职目标</span>为：WEB前端开发</p>
+        <p class="intro">自学前端技术14个月，期间制作多个小项目，能熟练运用HTML，CSS，Javascript制作网页，具有良好的编码习惯，熟悉面向对象。热爱前端技术，希望从事前端开发，<span class="label label-info">求职目标</span>为：WEB前端开发</p>
 
         <div data-role="collapsible-set" data-theme="c" data-content-theme="d" class="ui-collapsible-set">
 
             <div data-role="collapsible" data-inset="false" data-content-theme="c" data-iconpos="right" data-collapsed-icon="arrow-r" data-expanded-icon="arrow-d" id="skills">
                 <h2 class="title">个人技能<small>Skill</small></h2>
                 <ul>
-                    <li>熟练运用<strong>HTML</strong>、CSS设计和美化网页，了解W3C标准，正在学习CSS3，HTML5；</li>
+                    <li>熟练运用<strong>HTML</strong>、<strong>CSS</strong>设计和美化网页，了解W3C标准，正在学习CSS3，HTML5；</li>
                     <li>熟悉<strong>Javascript</strong>及开源库jQuery，正在学习NodeJS；</li>
-                    <li>了解BEM ,<strong>photoshop</strong>；</li>
-                    <li>了解<strong>PHP</strong>的简单使用,正在学习PHP；</li>
-                    <li>熟悉前端<strong>调试工具</strong>Firebug、Chrome；</li>
-                    <li>熟悉<strong>版本控制</strong>系统Subversion的配置使用，了解github和googlecode等服务；</li>
-                    <li>熟悉<strong>面向对象</strong>和<strong>设计模式</strong>，具有良好代码编写和<strong>重构习惯</strong>，了解敏捷开发理念和实践；</li>
-                    <li>良好的<strong>时间管理</strong>、<strong>任务管理</strong>能力</li>
+                    <li>了解<strong>BEM</strong>命名规则，了解LESS和SASS等预处理工具；</li>
+                    <li>了解<strong>PHP</strong>的基本语法，能用PHP编写简单的动态网页；</li>
+                    <li>熟练运用<strong>Sublime</strong>编写代码、<strong>Chrome</strong>调试代码；</li>
+                    <li>熟悉<strong>版本控制</strong>系统SVN和Git的使用，了解Github和GoogleCode等服务；</li>
+                    <li>能用<strong>Photoshop</strong>进行简单的平面设计和图像处理。</li>
                 </ul>
             </div>
 
@@ -46,11 +41,13 @@
                 <h2 class="title">项目经历<small>Projects</small></h2>
                 <div class="subsection">
                     <h3>
-                        <span class="date span3">2013.04</span>
-                        <span class="company">语音计算器</span>
+                        <span class="date span3">2013.07</span>
+                        <span class="company">类Wunderlist日程管理应用</span>
                     </h3>
                     <ul>
-                        <li>使用的语言和技术：HTML、CSS、Javascript</li>
+                        <li>可方便用其进行待办事项的添加、编辑、删除（支持批量操作）；</li>
+                        <li>使用Ajax进行数据接口调用，使用jQuery进行元素选取和事件处理；</li>
+                        <li>使用的语言和技术：HTML5、CSS、Javascript、Ajax、jQuery。</li>
                     </ul>
                 </div>
                 <div class="subsection">
@@ -59,7 +56,20 @@
                         <span class="company">数独游戏</span>
                     </h3>
                     <ul>
-                        <li>使用的语言和技术：HTML、CSS、Javascript</li>
+                        <li>利用回溯算法生成可解数独，并按难易程度挖空数独用于用户填写；</li>
+                        <li>用户填写时根据数独游戏规则验证输入是否正确，并验证游戏是否完成；</li>
+                        <li>使用的语言和技术：HTML、CSS、Javascript。</li>
+                    </ul>
+                </div>
+                <div class="subsection">
+                    <h3>
+                        <span class="date span3">2013.04</span>
+                        <span class="company">语音计算器</span>
+                    </h3>
+                    <ul>
+                        <li>利用自行录制的声音和HTML5，实现计算器的语音发声；</li>
+                        <li>使用分割算法处理计算结果，并以标准模式读取计算结果；</li>
+                        <li>使用的语言和技术：HTML5、CSS、Javascript。</li>
                     </ul>
                 </div>
             </div>
@@ -74,7 +84,6 @@
             <div data-role="collapsible" data-inset="false" data-content-theme="c" data-iconpos="right" data-collapsed-icon="arrow-r" data-expanded-icon="arrow-d" id="evaluation">
                 <h2 class="title">自我评价<small>Evaluation</small></h2>
                 <ul>
-                    <li>强烈的上进心，做事稳重；</li>
                     <li>为人真诚、诚实守信，善于与人沟通、合作；</li>
                     <li>对技术的<span class="label label-info">浓厚兴趣</span>和<span class="label label-info">钻研精神</span>；</li>
                     <li>爱自学，并能够持之以恒。</li>
@@ -90,7 +99,7 @@
                     <a href="http://weibo.com/wangshikun2010" target="_blank">
                         <img src="img/weibo.png" alt="WeiBo" title="sina">
                     </a>
-                    <a href="http://wenda.hiall.com.cn/u/wangshijun" target="_blank">
+                    <a href="http://wenda.hiall.com.cn/u/wangshikun" target="_blank">
                         <img src="img/wenda.png" alt="技能展示平台" title="技能展示平台">
                     </a>
                     <a href="http://code.google.com/u/wangshikun2010/" target="_blank">
@@ -102,8 +111,8 @@
             <div data-role="collapsible" data-inset="false" data-content-theme="c" data-iconpos="right" data-collapsed-icon="arrow-r" data-expanded-icon="arrow-d" id="contact">
                 <h2 class="title">联系信息<small>Contact</small></h2>
                 <div class="row">
-                    <p>邮箱：<a href="mailto:wangshijun2010@gmail.com">wangshikun2010@gmail.com</a></p>
-                    <p>电话：<a href="tel:15811057083">188-1033-1153</a></p>
+                    <p>邮箱：<a href="mailto:wangshikun2010@gmail.com">wangshikun2010@gmail.com</a></p>
+                    <p>电话：<a href="tel:18810331153">188-1033-1153</a></p>
                 </div>
             </div>
 
